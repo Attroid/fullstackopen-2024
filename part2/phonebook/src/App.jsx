09e5907260b55334f4ff2c4d1856f4be7e9fafd6
Atte Koivukangas <atte.koivukangas@aalto.fi>
2024-04-06 16:23:37 +0300

    2.13: The Phonebook Step 8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,15 +2,15 @@ import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-import axios from "axios";
+import personService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    personService.getAll().then((persons) => {
+      setPersons(persons);
     });
   }, []);
 
@@ -24,8 +24,8 @@ const App = () => {
       number,
     };
 
-    axios.post("http://localhost:3001/persons", person).then((response) => {
-      setPersons(persons.concat(response.data));
+    personService.create(person).then((createdPerson) => {
+      setPersons(persons.concat(createdPerson));
     });
   };
 
