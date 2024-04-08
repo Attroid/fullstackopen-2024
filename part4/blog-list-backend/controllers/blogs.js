054868936c0f49dc96607386a4c78f5c710b7171
Atte Koivukangas <atte.koivukangas@aalto.fi>
2024-04-08 21:45:54 +0300

    4.17: Blog List Expansion, step 5

--- part4/blog-list-backend/controllers/blogs.js
+++ part4/blog-list-backend/controllers/blogs.js
@@ -1,14 +1,22 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogRouter.get("/", async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   res.json(blogs);
 });
 
 blogRouter.post("/", async (req, res) => {
+  const user = await User.findOne({});
+  req.body.user = user._id;
+
   const blog = new Blog(req.body);
   await blog.save();
+
+  user.blogs = user.blogs.concat(blog._id);
+  await user.save();
+
   res.status(201).json(blog);
 });
 
--- part4/blog-list-backend/controllers/users.js
+++ part4/blog-list-backend/controllers/users.js
@@ -26,7 +26,11 @@ usersRouter.post("/", async (req, res) => {
 });
 
 usersRouter.get("/", async (req, res) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+  });
   res.json(users);
 });
 
