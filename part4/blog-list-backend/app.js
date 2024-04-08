acee5a1eba30010a45537913d9181a7ada0eece5
Atte Koivukangas <atte.koivukangas@aalto.fi>
2024-04-08 21:15:31 +0300

    4.15: Blog List Expansion, step 3

--- part4/blog-list-backend/app.js
+++ part4/blog-list-backend/app.js
@@ -3,6 +3,7 @@ require("express-async-errors");
 const cors = require("cors");
 const middleware = require("./utils/middleware");
 const blogRouter = require("./controllers/blogs");
+const userRouter = require("./controllers/users");
 const config = require("./utils/config");
 const mongoose = require("mongoose");
 const logger = require("./utils/logger");
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogRouter);
+app.use("/api/users", userRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
