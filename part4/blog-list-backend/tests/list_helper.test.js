3827a0a4451ae1b04976b7cc3b108b586a79befe
Atte Koivukangas <atte.koivukangas@aalto.fi>
2024-04-08 20:03:07 +0300

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/blog-list-backend/tests/list_helper.test.js
+++ part4/blog-list-backend/tests/list_helper.test.js
@@ -63,3 +63,66 @@ describe("total likes", () => {
     assert.strictEqual(result, 90);
   });
 });
+
+describe("favorite blog", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithMultipleBlogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "661420945a84973aff0a8312",
+      title: "The Mythical Man-Month",
+      author: "Fred Brooks",
+      url: "https://www.cs.drexel.edu/~yc349/CS451/RequiredReadings/MythicalManMonth.pdf",
+      likes: 52,
+      __v: 0,
+    },
+    {
+      _id: "6614209a19aa9038cf1c7517",
+      title: "No Silver Bullet - Essence and Accident in Software Engineering",
+      author: "Edsger W. Dijkstra",
+      url: "https://worrydream.com/refs/Brooks_1986_-_No_Silver_Bullet.pdf",
+      likes: 33,
+      __v: 0,
+    },
+  ];
+
+  test("of empty list is undefined", () => {
+    const result = listHelper.favoriteBlog([]);
+    assert.strictEqual(result, undefined);
+  });
+
+  test("when list has only one blog, equals that blog", () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    assert.deepStrictEqual(result, {
+      title: listWithOneBlog[0].title,
+      author: listWithOneBlog[0].author,
+      likes: listWithOneBlog[0].likes,
+    });
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.favoriteBlog(listWithMultipleBlogs);
+    assert.deepStrictEqual(result, {
+      title: listWithMultipleBlogs[1].title,
+      author: listWithMultipleBlogs[1].author,
+      likes: listWithMultipleBlogs[1].likes,
+    });
+  });
+});
--- part4/blog-list-backend/utils/list_helper.js
+++ part4/blog-list-backend/utils/list_helper.js
@@ -6,7 +6,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined;
+  }
+
+  const mostLikes = Math.max(...blogs.map((blog) => blog.likes));
+  const blog = blogs.find((blog) => blog.likes === mostLikes);
+
+  return {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
