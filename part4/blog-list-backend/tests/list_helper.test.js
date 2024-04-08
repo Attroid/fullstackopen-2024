11d7b38fb01397574185a8d48458304f03539d3f
Atte Koivukangas <atte.koivukangas@aalto.fi>
2024-04-08 19:54:56 +0300

    4.4: Helper Functions and Unit Tests, step 2

--- part4/blog-list-backend/tests/list_helper.test.js
+++ part4/blog-list-backend/tests/list_helper.test.js
@@ -8,3 +8,58 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithMultipleBlogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "661420945a84973aff0a8312",
+      title: "The Mythical Man-Month",
+      author: "Fred Brooks",
+      url: "https://www.cs.drexel.edu/~yc349/CS451/RequiredReadings/MythicalManMonth.pdf",
+      likes: 52,
+      __v: 0,
+    },
+    {
+      _id: "6614209a19aa9038cf1c7517",
+      title: "No Silver Bullet - Essence and Accident in Software Engineering",
+      author: "Edsger W. Dijkstra",
+      url: "https://worrydream.com/refs/Brooks_1986_-_No_Silver_Bullet.pdf",
+      likes: 33,
+      __v: 0,
+    },
+  ];
+
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs);
+    assert.strictEqual(result, 90);
+  });
+});
