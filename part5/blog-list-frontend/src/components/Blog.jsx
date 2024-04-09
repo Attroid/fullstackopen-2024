4a60d993769e758b924b8f957e338801771a8984
Atte Koivukangas <atte.koivukangas@aalto.fi>
2024-04-09 19:18:44 +0300

    5.7 Blog List Frontend, step 7

--- part5/blog-list-frontend/src/components/Blog.jsx
+++ part5/blog-list-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [open, setOpen] = useState(false);
+
+  const wrapperStyle = {
+    border: "1px solid",
+    padding: 16,
+    margin: "8px 0",
+    borderRadius: 5,
+  };
+
+  const toggleButtonStyle = {
+    padding: "8px 16px",
+    marginRight: 32,
+  };
+
+  return (
+    <div style={wrapperStyle}>
+      <button style={toggleButtonStyle} onClick={() => setOpen(!open)}>
+        {open ? "hide" : "view"}
+      </button>
+      {blog.title} {blog.author}
+      {open && (
+        <>
+          <hr />
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
